578cf07b2655c94e2988e4db3d549500b5b32aa1
arunm1000 <157619218+arunm1000@users.noreply.github.com>
2024-01-24 09:48:01 -0600

    Update streamlit_app.py again

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 
 import streamlit
 
-streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Oatmeal')
-streamlit.text('Wholegrain Bread Sandwich')
-streamlit.text('Kale smoothies')
+streamlit.title('My Parents new healthy diner') 
+streamlit.header('<font color=blue>Breakfast Menu</font>')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Avocado Toast')
