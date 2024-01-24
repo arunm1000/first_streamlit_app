8744b85fbb79cd66737a13a5c82f332cf0618166
arunm1000 <157619218+arunm1000@users.noreply.github.com>
2024-01-24 09:48:25 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit
 
 streamlit.title('My Parents new healthy diner') 
-streamlit.header('<font color=blue>Breakfast Menu</font>')
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞Hard-Boiled Free-Range Egg')
