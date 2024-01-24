9cb21fa9396cb2a8278ef58aa365bc28c0efb79f
arunm1000 <157619218+arunm1000@users.noreply.github.com>
2024-01-24 09:42:43 -0600

    Update streamlit_app.py with breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Oatmeal')
+streamlit.text('Wholegrain Bread Sandwich')
+streamlit.text('Kale smoothies')
