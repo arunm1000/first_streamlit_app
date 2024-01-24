ade9622b200f6a7e69086cb8c10a133d0729d509
arunm1000 <157619218+arunm1000@users.noreply.github.com>
2024-01-24 09:53:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ streamlit.text('🥑Avocado Toast')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-/* After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page by typing: */
+// After pulling the data into a pandas dataframe called my_fruit_list, we will ask the streamlit library to display it on the page by typing: */
 
 streamlit.dataframe(my_fruit_list)
